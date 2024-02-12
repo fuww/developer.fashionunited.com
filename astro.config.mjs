dbdf82a0cc109a3eb10d02476d4379e051946687
Joost van der Laan <joost@fashionunited.com>
2024-02-12 10:51:44 +0000

    content + menu

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,15 +7,38 @@ export default defineConfig({
 		starlight({
 			title: 'FashionUnited Docs',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/fuww/developers.fashionunited.com',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Docs',
 					items: [
 						// Each item here is one entry in the navigation menu.
 						{ label: 'Example Guide', link: '/guides/example/' },
-						{ label: 'Advertising', link: '/advertising/' },
+						{ label: 'Advertising', link: '/docs/advertising/' },
+						{ label: 'Editorial Cheat Sheet', link: '/docs/editorial-cheat-sheet/' },
+						{ label: 'Editorial Style Guide', link: '/docs/editorial-style-guide/' },
+						{ label: 'Fashionunited for Websites - Embedding Jobs and News', link: '/docs/fashionunited-for-websites/' },
+						{ label: 'Header', link: '/docs/header/' },
+						{ label: 'Jobs', link: '/docs/jobs/' },
+						{ label: 'Logo', link: '/docs/logo/' },
+						{ label: 'Marketplace', link: '/docs/marketplace/' },
+						{ label: 'Getting started - Marketplace', link: '/docs/marketplace/getting-started/' },
+						{ label: 'Brand Pages - Marketplace', link: '/docs/marketplace/brand-pages/' },
+						{ label: 'Categories - Marketplace', link: '/docs/marketplace/categories/' },
+						{ label: 'Colors - Marketplace', link: '/docs/marketplace/colors/' },
+						{ label: 'GraphQL API - Marketplace', link: '/docs/marketplace/graphql-api/' },
+						{ label: 'How to register - Marketplace', link: '/docs/marketplace/how-to-register/' },
+						{ label: 'Platforms - Marketplace', link: '/docs/marketplace/platforms/' },
+						{ label: 'Style Guide', link: '/docs/style-guide/' },
+						{ label: 'System Requirements and Browsers', link: '/docs/system-requirements-and-browsers/' },
+						{ label: 'Cloudflare', link: '/docs/cloudflare/' }
+					],
+				},
+				{
+				label: 'Guides',
+					items: [
+						{ label: 'Example Guide', link: '/guides/example/' },
 					],
 				},
 				{
--- deno/README.md
+++
@@ -1,3 +0,0 @@
-# developer.fashionunited.com
-
-Based on [Deno SaaSKit](https://deno.com/saaskit) - [Github repo](https://github.com/denoland/saaskit)
