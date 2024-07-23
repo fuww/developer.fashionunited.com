f42856d8c01bed40579e0c469f095ef2ddeda71b
Joost van der Laan <joost@fashionunited.com>
2024-07-23 21:21:10 +0000

    add Plausible

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,29 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-
 import tailwind from "@astrojs/tailwind";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://developer.fashionunited.com',
   integrations: [starlight({
     title: 'FashionUnited Docs',
-	customCss: [
-        './src/tailwind.css',
-        './src/styles/custom.css',
-        '@fontsource/ibm-plex-mono/400.css',
-        '@fontsource/ibm-plex-mono/600.css',
-        '@fontsource-variable/inter',
-        '@fontsource-variable/lora',        
-      ],
+    customCss: ['./src/tailwind.css', './src/styles/custom.css', '@fontsource/ibm-plex-mono/400.css', '@fontsource/ibm-plex-mono/600.css', '@fontsource-variable/inter', '@fontsource-variable/lora'],
     social: {
       github: 'https://github.com/fuww/developer.fashionunited.com'
     },
+    head: [
+      {
+        tag: "script",
+        attrs: {
+          type: "text/partytown",
+          src: "https://plausible.io/js/script.js",
+          "data-domain": "developer.fashionunited.com",
+          defer: true,
+        },
+      },
+    ],
     sidebar: [{
       label: 'Docs',
       items: [
@@ -50,36 +55,29 @@ export default defineConfig({
       autogenerate: {
         directory: 'docs/marketplace'
       }
-    },
-	{
-		label: 'Editorial',
-		items: [
-		{
-			label: 'Editorial Cheat Sheet',
-			link: '/docs/editorial-cheat-sheet/'
-		  }, {
-			label: 'Editorial Style Guide',
-			link: '/docs/editorial-style-guide/'
-		  },	],
-	  },
-	  {
-		label: 'This site',
-		items: [
-			{
-				label: 'Style Guide',
-				link: '/docs/style-guide/'
-			  },
-			],
-	  },
+    }, {
+      label: 'Editorial',
+      items: [{
+        label: 'Editorial Cheat Sheet',
+        link: '/docs/editorial-cheat-sheet/'
+      }, {
+        label: 'Editorial Style Guide',
+        link: '/docs/editorial-style-guide/'
+      }]
+    }, {
+      label: 'This site',
+      items: [{
+        label: 'Style Guide',
+        link: '/docs/style-guide/'
+      }]
+    }
     // {
     // 	label: 'Posts',
     // 	autogenerate: { directory: 'posts' },
     // },
     ]
-  }), 
-  tailwind({
-	// Disable the default base styles:
-	applyBaseStyles: false,
-  })
-]
+  }), tailwind({
+    // Disable the default base styles:
+    applyBaseStyles: false
+  }), partytown()]
 });
