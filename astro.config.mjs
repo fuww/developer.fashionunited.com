1f4d8186293119a38c1df89c4ccc4fc29d88b7a0
Joost van der Laan <joost@fashionunited.com>
2024-07-24 09:40:41 +0000

    Add View Transitions #45

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,74 +11,79 @@ export default defineConfig({
     remotePatterns: [{ protocol: "https" }],
   },
   site: 'https://developer.fashionunited.com',
-  integrations: [starlight({
-    title: 'FashionUnited Docs',
-    customCss: ['./src/tailwind.css', './src/styles/custom.css', '@fontsource/ibm-plex-mono/400.css', '@fontsource/ibm-plex-mono/600.css', '@fontsource-variable/inter', '@fontsource-variable/lora'],
-    social: {
-      github: 'https://github.com/fuww/developer.fashionunited.com'
-    },
-    head: [
-      {
-        tag: "script",
-        attrs: {
-          type: "text/partytown",
-          src: "https://plausible.io/js/script.js",
-          "data-domain": "developer.fashionunited.com",
-          defer: true,
-        },
+  integrations: [
+    starlight({
+      components: {
+        Head: "./src/components/starlight/Head.astro",
+        Header: "./src/components/starlight/Header.astro",
       },
-    ],
-    sidebar: [{
-      label: 'Docs',
-      items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: 'Advertising',
-        link: '/docs/advertising/'
+      title: 'FashionUnited Docs',
+      customCss: ['./src/tailwind.css', './src/styles/custom.css', '@fontsource/ibm-plex-mono/400.css', '@fontsource/ibm-plex-mono/600.css', '@fontsource-variable/inter', '@fontsource-variable/lora'],
+      social: {
+        github: 'https://github.com/fuww/developer.fashionunited.com'
+      },
+      head: [
+        {
+          tag: "script",
+          attrs: {
+            type: "text/partytown",
+            src: "https://plausible.io/js/script.js",
+            "data-domain": "developer.fashionunited.com",
+            defer: true,
+          },
+        },
+      ],
+      sidebar: [{
+        label: 'Docs',
+        items: [
+        // Each item here is one entry in the navigation menu.
+        {
+          label: 'Advertising',
+          link: '/docs/advertising/'
+        }, {
+          label: 'FashionUnited for Websites - Embedding Jobs and News',
+          link: '/docs/fashionunited-for-websites/'
+        }, {
+          label: 'Header',
+          link: '/docs/header/'
+        }, {
+          label: 'Jobs',
+          link: '/docs/jobs/'
+        }, {
+          label: 'Logo',
+          link: '/docs/logo/'
+        }, {
+          label: 'System Requirements and Browsers',
+          link: '/docs/system-requirements-and-browsers/'
+        }, {
+          label: 'Cloudflare',
+          link: '/docs/cloudflare/'
+        }]
       }, {
-        label: 'FashionUnited for Websites - Embedding Jobs and News',
-        link: '/docs/fashionunited-for-websites/'
+        label: 'Marketplace',
+        autogenerate: {
+          directory: 'docs/marketplace'
+        }
       }, {
-        label: 'Header',
-        link: '/docs/header/'
+        label: 'Editorial',
+        items: [{
+          label: 'Editorial Cheat Sheet',
+          link: '/docs/editorial-cheat-sheet/'
+        }, {
+          label: 'Editorial Style Guide',
+          link: '/docs/editorial-style-guide/'
+        }]
       }, {
-        label: 'Jobs',
-        link: '/docs/jobs/'
-      }, {
-        label: 'Logo',
-        link: '/docs/logo/'
-      }, {
-        label: 'System Requirements and Browsers',
-        link: '/docs/system-requirements-and-browsers/'
-      }, {
-        label: 'Cloudflare',
-        link: '/docs/cloudflare/'
-      }]
-    }, {
-      label: 'Marketplace',
-      autogenerate: {
-        directory: 'docs/marketplace'
+        label: 'This site',
+        items: [{
+          label: 'Style Guide',
+          link: '/docs/style-guide/'
+        }]
       }
-    }, {
-      label: 'Editorial',
-      items: [{
-        label: 'Editorial Cheat Sheet',
-        link: '/docs/editorial-cheat-sheet/'
-      }, {
-        label: 'Editorial Style Guide',
-        link: '/docs/editorial-style-guide/'
-      }]
-    }, {
-      label: 'This site',
-      items: [{
-        label: 'Style Guide',
-        link: '/docs/style-guide/'
-      }]
-    }
-    // {
-    // 	label: 'Posts',
-    // 	autogenerate: { directory: 'posts' },
-    // },
+      // {
+      // 	label: 'Posts',
+      // 	autogenerate: { directory: 'posts' },
+      // },
     ]
   }), tailwind({
     // Disable the default base styles:
